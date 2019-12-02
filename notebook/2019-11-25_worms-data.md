4b434e6fef7669881597afc0bb92071ef13ec8a4
bweedop <kbweedop@gmail.com>
2019-12-02 10:31:38 -0500

    data and figures added for copper and w/o copper

--- notebook/2019-11-25_worms-data.md
+++ notebook/2019-11-25_worms-data.md
@@ -1,12 +1,5 @@
 # 2019-11-25 - Hidden Markov Model for C. elegans behavior
 
-After digging into the data initially, I have some questions.
+Should we have a transition matrix between reorientations and crawls? Do C. elegans transition between these two classes of behaviors at different probabilities?
 
-1. The behavior codes are a bit ambiguous (or, at least it is not obvious to me what some of them are meant to be). 
-    - The column `beh1_name` has both character and numerical categorical identifiers:
-    ```{r}
-    > unique(worms.data$beh1_name)
- [1] "NaN"    "rev"    "oparc"  "pir"    "line"   "pauses" "om"     "loop"  
- [9] "clarc"  "93"     "83"     "94"     "92"     "91"     "82"     "84"    
-[17] "81" 
-    ```
+What is the initial transition matrix? 
--- src/hidden-worms.R
+++ src/hidden-worms.R
@@ -13,7 +13,7 @@ worms.data <- read.csv("./data/table_matrix_p_line_atr_tded.csv",
 # Processing the data
 data.cp <- worms.data[, 2:9]
 copper.codes <- c("NaN", "81", "82", "83", "84", "91", "92", "93", "94")
-states <- c("crawl", "reorient", "copper_beh")
+# states <- c("crawl", "reorient", "copper_ring")
 data.cp <- data.cp[-which(data.cp$beh1_name %in% copper.codes), ]
 behaviors <- unique(data.cp$beh1_name)
 for (i in 1:nrow(data.cp)) {
@@ -88,7 +88,7 @@ for(i in 1:length(sim.worm$observation)) {
 }
 
 # Plot simulated worm behavior as a discrete function of time
-png("figures/sim-worm-behavior.png", width = 1200, height = 480,
+png("figures/sim-worm-behavior_no-copper.png", width = 1200, height = 480,
     units = "px", pointsize = 12, bg = "white", res = NA)
 plot(seq(1, 1289), 
      sim.worm$obs.code,
@@ -97,3 +97,101 @@ plot(seq(1, 1289),
      main = "Simulated Worm",
      type = "l")
 dev.off()
+
+write.csv(bw$hmm$emissionProbs,
+          "data/hmm-emission-matrix_no-copper.csv")
+
+################################################################################
+# Include Copper behaviors
+################################################################################
+
+# Processing the data
+data.cp <- worms.data[, 2:9]
+copper.codes <- c("NaN", "81", "82", "83", "84", "91", "92", "93", "94")
+# states <- c("crawl", "reorient", "copper_ring")
+data.cp$beh1_name[which(data.cp$beh1_name %in% copper.codes)] <- "copper_beh"
+behaviors <- unique(data.cp$beh1_name)
+for (i in 1:nrow(data.cp)) {
+    data.cp$beh1_num[i] <- which(behaviors == data.cp$beh1_name[i])
+}
+
+# Plot worm #1 behavior as a discrete function of time
+png("./figures/worm-1-behavior.png", width = 1200, height = 480,
+    units = "px", pointsize = 12, bg = "white", res = NA)
+plot(data.cp$frame[which(data.cp$worm_ID == 1)], 
+     data.cp$beh1_num[which(data.cp$worm_ID == 1)],
+     xlab = "Frame",
+     ylab = "Behavior (Code)",
+     main = "Worm_ID => 1",
+     type = "l")
+dev.off()
+
+# trans.mat - Transition matrix for the states of our hmm. These values are 
+#   established using the data that we have from Dr. Salvador's experiments.
+
+trans.mat <- matrix(NA, 
+                    length(behaviors), 
+                    length(behaviors), 
+                    dimnames = list(behaviors, behaviors))
+
+# tmp and total are counters for the number of transitions from one behavior to 
+#   another and the total number of transitions between one behavior to all 
+#   others, respectively.
+tmp <- 0
+total <- 0
+# Loop through data and count the number of transitions from one behavior to 
+# another (including itself).
+for (g in 1:length(behaviors)) {
+    for (h in 1:length(behaviors)) {
+        # Do each worm separately
+        for (i in unique(data.cp$worm_ID)) {
+            # Remove the last frame from the loop
+            for (j in which(data.cp$worm_ID == i)[-length(which(data.cp$worm_ID == i))]) {
+                # increase total count if behavior in focus seen
+                if (data.cp$beh1_name[j] == behaviors[h]) {
+                    total <- total + 1
+                }
+                # increase tmp count if behavior in focus transitions to 2nd behavior in focus
+                if (data.cp$beh1_name[j] == behaviors[h] && data.cp$beh1_name[j+1] == behaviors[g]) {
+                    tmp <- tmp + 1
+                }
+            }
+        }
+        # Divide tmp by total to get probability of the transition in focus happening given the data
+        trans.mat[g, h] <- tmp / total
+        # Reset tmp and total
+        total <- 0
+        tmp <- 0
+    }
+}
+
+# Initial HMM
+hmm <- initHMM(behaviors, behaviors, transProbs = trans.mat)
+print(hmm)
+
+# Sequence of observation; Do each separately
+for (i in unique(data.cp$worm_ID)[2]) {
+    observation <- data.cp$beh1_name[which(data.cp$worm_ID == i)]
+    bw <- baumWelch(hmm, observation, 10)
+}
+
+# Simulating worm behavior given our model
+sim.worm <- simHMM(bw$hmm, 1289)
+sim.worm$obs.code <- rep(NA, 1289)
+for(i in 1:length(sim.worm$observation)) {
+    sim.worm$obs.code[i] <- which(behaviors == sim.worm$observation[i])
+}
+
+# Plot simulated worm behavior as a discrete function of time
+png("figures/sim-worm-behavior_copper.png", width = 1200, height = 480,
+    units = "px", pointsize = 12, bg = "white", res = NA)
+plot(seq(1, 1289), 
+     sim.worm$obs.code,
+     xlab = "Frame",
+     ylab = "Behavior (Code)",
+     main = "Simulated Worm",
+     type = "l")
+dev.off()
+
+write.csv(bw$hmm$emissionProbs,
+          "data/hmm-emission-matrix_with-copper.csv")
